f0b3b6b1071506b370ad173b6fe361a7bdd43d3b
63361dcfecfd34092cd96903469965079ab284d6 <f.ottavi@ipzs.it>
2018-01-18 15:27:27 +0100

    remove unused dir

--- CacheLib_keys/CacheLib.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include <vector>
-
-bool CacheExists(const char *PAN);
-void CacheGetCertificate(const char *PAN, std::vector<BYTE>&certificate);
-void CacheGetPIN(const char *PAN, std::vector<BYTE>&PIN);
-void CacheSetData(const char *PAN, BYTE *certificate, int certificateSize, BYTE *FirstPIN, int FirstPINSize);
-
